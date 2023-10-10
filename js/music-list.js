c2dd9ecf7f37c875d5cb05338dd469226ff0ca52
Vedaant Singh <vedaantsingh@vedaants-MacBook-Pro.local>
2023-10-10 18:56:45 +0100

    first commit

---
+++ js/music-list.js
@@ -0,0 +1,85 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Tum Ho",
+    artist: "Mohit Chauhan",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Tere Bina",
+    artist: "A.R Rahman",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Tu Hi Meri Shabh",
+    artist: "KK",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Aaoge Jab Tum",
+    artist: "Ustad Rashid Khan",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Tune Kaha",
+    artist: "Prateek Kuhad",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Kyun",
+    artist: "Papon",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Zara Zara Bekhta Hai",
+    artist: "Bombay Jayshree",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Kho Gaye Hum Kaha",
+    artist: "Jasleen, Prateek Kuhad",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "Tujhe Kitna Chahne Lage",
+    artist: "Arjit Singh",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "Pehla Nasha",
+    artist: "Udit Narayan",
+    img: "music-10",
+    src: "music-10"
+  },
+  {
+    name: "Kal Ho Naa Ho",
+    artist: "Sonu Nigam",
+    img: "music-11",
+    src: "music-11"
+  },
+  {
+    name: "Aaj Kal Zindagi",
+    artist: "Shankar Mahadevan",
+    img: "music-12",
+    src: "music-12"
+  },
+];
